e89c5d60ad2ca797b11e8e8f57e6907024264bd4
bryansis2010 <bryan.sis.2010@gmail.com>
2015-02-21 17:07:31 +0800

    Completed Chapter 2.2

--- chap02/chap02.py
+++ chap02/chap02.py
@@ -6,9 +6,12 @@
 # Created:     21/02/2015
 #-------------------------------------------------------------------------------
 
-#imports from the textbook
+#imports relevant to the text
 from numpy import *
 import operator
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.legend_handler
 
 #my own imports to make things easier
 import os
@@ -25,14 +28,14 @@ def classify0(in_X, data_set, labels, k):
 
     #Find out the no. of elements in the data_set
     data_set_size = data_set.shape[0]
+    """Covered in the explanation"""
     diff_matrix = tile(in_X, (data_set_size, 1)) - data_set
     sq_diff_matrix = diff_matrix**2
     sq_distances = sq_diff_matrix.sum(axis=1)
     distances = sq_distances**0.5
     sorted_dist_indices = distances.argsort()
-    print(sorted_dist_indices)
-    class_histogram = {}
 
+    class_histogram = {}
     for i in range(k):
         vote_label = labels[sorted_dist_indices[i]]
         class_histogram [vote_label] = class_histogram.get(vote_label, 0) + 1
@@ -56,17 +59,110 @@ def file2matrix(filename):
     for line in fr.readlines():
         line = line.strip()
         list_from_line = line.split('\t')
+        #substitutes the row of 0s to values from the text file
         returned_matrix [index, :] = list_from_line[0:3]
+        #adds the label using the last entry of the row.
         class_label_vector.append(int(list_from_line[-1]))
         index += 1
     return returned_matrix, class_label_vector
 #end file2matrix
 
 
-"""Example 2.1"""
+def autoNorm(data_set):
+    min_vals = data_set.min(0)
+    max_vals = data_set.max(0)
+    ranges = max_vals - min_vals
+    normalised_data_set = zeros(shape(data_set))
+    num_records = data_set.shape[0]
+    normalised_data_set = data_set - tile(min_vals, (num_records, 1))
+    normalised_data_set = normalised_data_set/tile(ranges, (num_records, 1))
+    return normalised_data_set, ranges, min_vals
+#end autoNorm
+
+def datingClassTest():
+    resource_path = os.path.dirname(__file__)
+    file_name = 'resource\datingTestSet2.txt'
+    dating_test_txtfile = os.path.join(resource_path, file_name)
+
+    hoRatio = 0.10
+    dating_data_matrix, dating_labels = file2matrix(dating_test_txtfile)
+    normalized_data_set, ranges, min_vals = autoNorm(dating_data_matrix)
+    num_records = normalized_data_set.shape[0]
+    num_test_vectors = int(num_records*hoRatio)
+    error_count = 0.0
+
+    for i in range(num_test_vectors):
+        classifier_result = classify0(normalized_data_set[i,:],
+        normalized_data_set[num_test_vectors:num_records, :],
+        dating_labels[num_test_vectors:num_records], 3)
+        print("the classifier came back with : %d, the real answer is %d"
+        %(classifier_result, dating_labels[i]))
+        if(classifier_result != dating_labels[i]):
+            error_count += 1.0
+            #print("Error!")
+    print("the total error rate is : %f" %(error_count/float(num_test_vectors)))
+#end datingClassTest
+
+def classifyPerson():
+    resource_path = os.path.dirname(__file__)
+    file_name = 'resource\datingTestSet2.txt'
+    dating_test_txtfile = os.path.join(resource_path, file_name)
+
+    result_list = ["not at all", "in small doses", "in large doses"]
+    pct_video_games = float(input("percentage of time spent playing video games?"))
+    num_flier_miles = float(input("frequent flier miles earned per year?"))
+    ltr_ice_cream = float(input("liters of ice cream consumed per week?"))
+    dating_data_matrix, dating_labels = file2matrix(dating_test_txtfile)
+    normalized_data_set, ranges, min_vals = autoNorm(dating_data_matrix)
+    input_vector = array([num_flier_miles, pct_video_games, ltr_ice_cream])
+    input_vector = (input_vector-min_vals)/ranges
+    classifierResult = classify0(input_vector,normalized_data_set,
+    dating_labels,3)
+    print ("You will probably like this person: %s" % result_list[classifierResult - 1])
+
+
+"""Example 2.1
 group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
 labels = ['A','A','B','B']
-print(classify0([0.6, 0.6], group, labels, 3))
+print(classify0([0.6, 0.6], group, labels, 3))"""
 
-"""Example 2.2.1"""
-datingDataMat, datingLabels = file2matrix
+"""Example 2.2.1
+resource_path = os.path.dirname(__file__)
+file_name = 'resource\datingTestSet2.txt'
+dating_test_txtfile = os.path.join(resource_path, file_name)
+dating_data_matrix, dating_labels = file2matrix(dating_test_txtfile)"""
+
+"""Example 2.2.2 - Fig 2.3 (use with Eg. 2.2.1)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(dating_data_matrix[:, 1], dating_data_matrix[:, 2])
+ax.set_xlabel("Percentage of Time Spent Playing Video Games")
+ax.set_ylabel("Liters of Ice Cream Consumed Per Week")
+plt.show()"""
+
+"""Example 2.2.2 - Fig 2.4 (use with Eg. 2.2.1)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(dating_data_matrix[:, 1], dating_data_matrix[:, 2],
+15.0*array(dating_labels), 15.0*array(dating_labels))
+ax.set_xlabel("Percentage of Time Spent Playing Video Games")
+ax.set_ylabel("Liters of Ice Cream Consumed Per Week")
+plt.show()"""
+
+"""Example 2.2.2 - Fig 2.5 (use with Eg. 2.2.1)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(dating_data_matrix[:, 0], dating_data_matrix[:, 1],
+15.0*array(dating_labels), 15.0*array(dating_labels))
+ax.set_xlabel("Frequent Flier Miles Earned per Year")
+ax.set_ylabel("Percentage of Time Spent Playing Video Games")
+plt.show()"""
+
+"""Example 2.2.3
+normalized_data_set, ranges, min_vals = autoNorm(dating_data_matrix)"""
+
+"""Example 2.2.4
+datingClassTest()"""
+
+"""Example 2.2.5"""
+classifyPerson()
