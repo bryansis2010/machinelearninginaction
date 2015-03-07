8b389c131eda717e824d037a9b5f716ddcc7c161
bryansis2010 <bryan.sis.2010@gmail.com>
2015-03-07 18:27:27 +0800

    Started for Chapter 4

---
+++ chap04/chap04.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        chap04
+#
+# Author:      bryansis2010
+#
+# Created:     07/03/2015
+#-------------------------------------------------------------------------------
+
+#imports relevant to the text
+from numpy import *
+import operator
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.legend_handler
+
+#my own imports to make things easier
