f21a2af2c306d5c53d56d5a7bcd8a5f2f07fea1b
bryansis2010 <bryan.sis.2010@gmail.com>
2015-04-05 22:50:07 +0800

    Added read file

--- chap05/chap05.py
+++ chap05/chap05.py
@@ -5,3 +5,22 @@
 #
 # Created:     05/04/2015
 #-------------------------------------------------------------------------------
+#imports relevant to the text
+from numpy import *
+import re
+#my own imports to make things easier
+import os
+
+resource_path = os.path.dirname(__file__)
+file_name = r"resource\testSet.txt"
+testSet_txtfile = ("%s\%s" % (resource_path, file_name))
+testSet_fh = open(testSet_txtfile)
+testSet_text = testSet_fh.readlines()
+
+dataMat = []; labelMat = []
+for line in testSet_text:
+    line_array = line.split("\t")
+    dataMat.append([1.0, float(line_array[0]), float(line_array[1])])
+    labelMat.append(int(line_array[-1]))
+
+#print(testSet_text)
